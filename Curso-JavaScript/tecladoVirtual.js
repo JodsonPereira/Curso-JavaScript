f9966b044436c002d530c2f10975cf3bcd737264
Jodson Pereira <jodsonsenai@gmail.com>
2023-05-22 19:07:53 -0300

    atualizando dados dia 22/05/2023 19:07

---
+++ Curso-JavaScript/tecladoVirtual.js
@@ -0,0 +1,7 @@
+function aperta(lt){
+    document.getElementById("aperta").innerHTML = `Você apertou a letra ${lt}`;
+}
+
+function solta(lt){
+    document.getElementById("solta").innerHTML = `Voce soltou a tecla ${lt}`;
+}
